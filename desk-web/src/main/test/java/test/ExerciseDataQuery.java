4a3853b5b845acc0ecb08a377df8004866f077ad
ZhangYubin <13608428279@163.com>
2019-05-23 13:25:04 +0800

    2019/5/22

---
+++ desk-web/src/main/test/java/test/ExerciseDataQuery.java
@@ -0,0 +1,69 @@
+package test;
+
+public class ExerciseDataQuery {
+    private int numid;
+    private String xuehao;
+    private String name;
+    private String cardId;
+    private String daTe;
+    private String dateTime;
+    private String isvalid;
+
+
+    public int getNumid() {
+        return numid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumid(int numid) {
+        this.numid = numid;
+    }
+
+    public String getXuehao() {
+        return xuehao;
+    }
+
+    public void setXuehao(String xuehao) {
+        this.xuehao = xuehao;
+    }
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
+    }
+
+    public String getDaTe() {
+        return daTe;
+    }
+
+    public void setDaTe(String daTe) {
+        this.daTe = daTe;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getIsvalid() {
+        return isvalid;
+    }
+
+    public void setIsvalid(String isvalid) {
+        this.isvalid = isvalid;
+    }
+
+}
--- desk-web/src/main/test/java/test/Test1.java
+++ desk-web/src/main/test/java/test/Test1.java
@@ -3,13 +3,13 @@ package test;
 import org.apache.http.cookie.Cookie;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Test1 {
     @Test
@@ -36,6 +36,55 @@ public class Test1 {
             String k = HttpClient.get(U2);
             Document d = Jsoup.parse(k);
             System.out.println(d.toString());
+
+            Element j = d.getElementById("tableHeader");
+        Elements jl = j.getElementsByClass("grid_header");
+        System.out.println(j.toString());
+        System.out.println(jl.toString());
+
+        for (int i = 0; i <jl.size() ; i++) {
+            //System.out.println(jl.get(i).attributes().toString());
+            System.out.println(jl.get(i).text());
+        }
+
+        Element ll = d.getElementById("dataTable");
+        Elements p = ll.getElementsByClass("grid_number");
+        Elements tt = ll.getElementsByClass("grid_data");
+        for (int i = 0; i < p.size(); i++) {
+            System.out.println(p.get(i).text());
+        }
+        ExerciseDataQuery exerciseDataQuery = null;
+        List<ExerciseDataQuery> exerciseDataQueryList = new ArrayList<>();
+        int kk = 1;
+        for (int i = 0; i <tt.size() ; i++) {
+
+            if (i%8 != 0 || i == 0  ){
+                exerciseDataQuery = new ExerciseDataQuery();
+                System.out.print(tt.get(i).text()+",");
+                exerciseDataQuery.setName(tt.get(i).text());
+                i++;
+                exerciseDataQuery.setCardId(tt.get(i).text());
+                i++;
+                exerciseDataQuery.setDaTe(tt.get(i).text());
+                i++;
+                exerciseDataQuery.setDateTime(tt.get(i).text());
+                i++;
+                exerciseDataQuery.setIsvalid(tt.get(i).text());
+                i++;
+                i++;
+
+                //exerciseDataQuery.setNumid(kk);
+                //kk++;
+            }else {
+                System.out.println();
+                exerciseDataQueryList.add(exerciseDataQuery);
+            }
+
+        }
+        System.out.println(exerciseDataQueryList.size());
+
+        //System.out.println(exerciseDataQueryList.get(1).getDateTime());
+
             //获取字段信息
            /*
             String _eventId = doc.select("input[name=_eventId]").attr("value");
